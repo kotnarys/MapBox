c95285ba1b26b986813d4f42204ccfd3f8ae2654
kotnarys <kotnarys@mail.ru>
2023-01-21 22:21:14 +0300

    add useRef, for container MapBox

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { render } from "react-dom";
 import useMapBox from "./useMapBox";
 import "./index.css";
 
 function App() {
+
+  const mapRef = useRef();
   const [random, setRandom] = useState(Math.random());
 
-  useMapBox({ id: "map", center: [3.0, 2.0], zoom: 5, random: random });
+  useMapBox({ ref: {mapRef}, center: [3.0, 2.0], zoom: 5, random: random });
   return (
     <>
-      <button className="btn" id="rerender" onClick={React.memo(() => setRandom(Math.random()))}>
+      <button ref={mapRef} className="btn" id="rerender" onClick={React.memo(() => setRandom(Math.random()))}>
         Ререндер!
       </button>
       <div  id="map"></div>
--- src/useMapBox.js
+++ src/useMapBox.js
@@ -2,9 +2,9 @@ import { useMemo, useLayoutEffect } from "react";
 import mapboxgl from "mapbox-gl";
 
 export default function useMapBox({
-  id = "map",
   center = [37.61192, 55.76199],
-  zoom = 10
+  zoom = 10, 
+  ref
 }) {
   mapboxgl.accessToken =
     "pk.eyJ1Ijoid3JremciLCJhIjoiY2w1MTRsMW41MDI0ejNkcnliczMyNmpjNyJ9.Oydx12NzpiCwXiQ8qKG9-Q";
@@ -15,7 +15,7 @@ export default function useMapBox({
 
   useLayoutEffect(() => {
     new mapboxgl.Map({
-      container: id,
+      container: ref.current,
       style: "mapbox://styles/mapbox/dark-v10",
       center: centerMemo,
       zoom: zoom
